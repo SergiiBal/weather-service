848de5016cc212a4474dc91cb324b3c42aa4229c
SergiiBal <sergey.balashchuk@gmail.com>
2022-09-17 22:35:31 +0300

    Second try

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("// making changes for commit");
+        System.out.println(printName("Ostap"));
+    }
+
+    public static String printName(String name) {
+        return "Hello " + name;
     }
 }
