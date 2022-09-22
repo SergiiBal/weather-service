6927077314b99cb9a6197f1f00e4eb2b2d409d97
Ostap Gonchar <ostap,gonchar@gmail.com>
2022-09-22 21:27:16 +0200

    Updated package

--- src/test/java/com/sergii/controllers/HelloControllerTest.java
+++ src/test/java/com/sergii/controllers/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.sergii.controllers;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
