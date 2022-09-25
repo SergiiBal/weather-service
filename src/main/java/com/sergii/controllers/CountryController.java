12fc5f1c2d61078cb59ce0ee0abfbd31718246b7
SergiiBal <sergey.balashchuk@gmail.com>
2022-09-25 23:17:45 +0300

    +Cash

--- src/main/java/com/sergii/controllers/CountryController.java
+++ src/main/java/com/sergii/controllers/CountryController.java
@@ -2,6 +2,7 @@ package com.sergii.controllers;
 
 import com.sergii.models.CountriesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -22,6 +23,7 @@ public class CountryController {
         this.restTemplate = restTemplate;
     }
 
+    @Cacheable("CountriesList")
     @GetMapping("/list")
     public String listCountries(@RequestParam(name = "key", defaultValue = "") String key) {
         HttpHeaders headers = new HttpHeaders();
@@ -29,13 +31,15 @@ public class CountryController {
         headers.set("X-RapidAPI-Host", "countries-cities.p.rapidapi.com");
         HttpEntity<?> requestEntity = new HttpEntity<>(headers);
 
+        System.out.println("Making Rest Call");
+
         ResponseEntity<CountriesResponse> responseEntity = restTemplate.exchange(
                 "https://countries-cities.p.rapidapi.com/location/country/list",
                 HttpMethod.GET,
                 requestEntity,
                 CountriesResponse.class);
         if (!key.isEmpty()) {
-            return responseEntity.getBody().countries().get(key);
+            return responseEntity.getBody().countries().get(key.toUpperCase());
         } else {
             return responseEntity.getBody().countries().toString();
         }
